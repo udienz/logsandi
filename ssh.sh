10c06982cad88d5e4c0b4117b1d270d9ac17982b
Mahyuddin Susanto <udienz@rad.net.id>
2013-01-30 18:37:25 +0700

    addd missing files

--- ssh.sh
+++ ssh.sh
@@ -2,28 +2,43 @@
 
 git pull origin master
 
-BASE=$HOME/logsandi/
+BASE=$HOME/logsandi
+
+blacklist=$BASE/daftarhitam.log
+whitelist=$BASE/daftarputih.log
+wrapper=/etc/hosts.deny
+
+if [ ! -f $BASE/daftarhitam.log ]; then
+        touch $BASE/daftarhitam.log
+fi
 
 sed -e '/sshd\[[0-9]*\]: Failed password/!d' \
     -e 's/.*Failed password for.*from //' \
     -e 's/ port.*//' /var/log/auth.log | sort | uniq -c | \
 while read count host
-	do
-	hostx=$(echo $host | sed -e 's/::ffff://')
-#	echo $host $count
-#	echo $hostx $count
+        do
+        hostx=$(echo $host | sed -e 's/::ffff://')
 
-	if grep "^$hostx" $BASE/daftarhitam.log ; then
-		echo "$hostx already blocked"
+if [ "$count" -gt "5" ]; then
+	if grep "^$hostx" $whitelist ; then
+		echo "host $hostx has whitelist"
+
 	else
-
-		if [ "$count" -gt "5" ]; then
-			echo "$hostx brute force $count"
-			echo "$hostx bf $count" >> $BASE/daftarhitam.log
+		if grep "^sshd: $hostx" $wrapper ; then
+			echo "already blocked at tcpwrapper"
+		else
+			echo "sshd: $hostx" >> $wrapper
+				if grep "^$hostx" $blacklist ; then
+					echo "$hostx already blocked"
+				else
+					echo "$hostx" >> $blacklist
+				fi
 		fi
 	fi
+fi
 done
 
 git add *
 git commit -m "autocommit at $(hostname)"
 git push origin master
+
